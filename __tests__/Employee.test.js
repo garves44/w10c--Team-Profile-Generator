aa5f8f5bd2faf1fe65363ad7ffad64202bbbf9e4
Garves <garves44@gmail.com>
2020-07-08 12:02:37 -0600

    added getName and getId test to Employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,7 +2,7 @@ const Employee = require("../lib/Employee");
 
 
 test("Creates Employee Obj", () => {
-    const employee = new Employee();
+    const employee = new Employee('Garves', '44', 'example@gmail.com')
 
     expect(employee).toEqual(expect.any(Object));
 });
@@ -14,3 +14,15 @@ test('Employee Params are being used properly', () => {
     expect(employee.id).toBe('44');
     expect(employee.email).toBe('example@gmail.com');
 })
+
+test('Get employee Name', () => {
+    const employee = new Employee('Garves', '44', 'example@gmail.com')
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()));
+});
+
+test('Get employee ID', () => {
+    const employee = new Employee('Garves', '44', 'example@gmail.com')
+
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));
+});
