d879f549154e07e3fed044af940aa6c5f955b372
Garves <garves44@gmail.com>
2020-07-08 12:16:26 -0600

    added tests for getGithub() and getRole() to Engineer.test.js

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -14,4 +14,16 @@ test('Engineer Params are being used properly', () => {
     expect(engineer.id).toBe('50');
     expect(engineer.email).toBe('example@engineering.com');
     expect(engineer.github).toBe('example@github.com');
-})
+});
+
+test('Get engineer github via getGithub()', () => {
+    const engineer = new Engineer('Garves', '50', 'example@engineering.com', 'example@github.com');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+test('Get engineer role', () => {
+    const engineer = new Engineer('Garves', '50', 'example@engineering.com', 'example@github.com');
+
+    expect(engineer.getRole()).toBe('Engineer');
+});
