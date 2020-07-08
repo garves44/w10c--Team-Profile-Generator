178a45ed0c6cdc1bbad0b4b9465ec6b6a11af5c6
Garves <garves44@gmail.com>
2020-07-08 12:25:01 -0600

    added test for getSchool() and getRole() to Intern.test.js

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -6,3 +6,24 @@ test('Creates Intern Obj', () => {
 
     expect(intern).toEqual(expect.any(Object));
 });
+
+test('Intern Params are being used properly', () => {
+    const intern = new Intern('Jeremy', '1', 'example@intern.com', 'UofU');
+
+    expect(intern.name).toBe('Jeremy');
+    expect(intern.id).toBe('1');
+    expect(intern.email).toBe('example@intern.com');
+    expect(intern.school).toBe('UofU');
+});
+
+test('Get intern school via getSchool()', () => {
+    const intern = new Intern('Jeremy', '1', 'example@intern.com', 'UofU');
+
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+test('Get intern role via getRole()', () => {
+    const intern = new Intern('Jeremy', '1', 'example@intern.com', 'UofU');
+
+    expect(intern.getRole()).toBe('Intern');
+});
