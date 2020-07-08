d437c7d82f2eb5c5aafbda09bdef55a634beeaff
Garves <garves44@gmail.com>
2020-07-08 12:13:05 -0600

    Engineer test to check params

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,8 +1,17 @@
 const Engineer = require("../lib/Engineer");
 
 
-test('Get Engineer Github', () => {
+test('Create Engineer Obj', () => {
     const engineer = new Engineer('Garves', '50', 'example@engineering.com', 'example@github.com');
 
     expect(engineer).toEqual(expect.any(Object));
 });
+
+test('Engineer Params are being used properly', () => {
+    const engineer = new Engineer('Garves', '50', 'example@engineering.com', 'example@github.com');
+
+    expect(engineer.name).toBe('Garves');
+    expect(engineer.id).toBe('50');
+    expect(engineer.email).toBe('example@engineering.com');
+    expect(engineer.github).toBe('example@github.com');
+})
