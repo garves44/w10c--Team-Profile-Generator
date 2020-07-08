416d0a4aaebbfa7e6f67c814bfcb3ddc2f16407d
Garves <garves44@gmail.com>
2020-07-08 11:57:58 -0600

    added test to employee to check if Params are being used properly

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,3 +6,11 @@ test("Creates Employee Obj", () => {
 
     expect(employee).toEqual(expect.any(Object));
 });
+
+test('Employee Params are being used properly', () => {
+    const employee = new Employee('Garves', '44', 'example@gmail.com')
+
+    expect(employee.name).toBe('Garves');
+    expect(employee.id).toBe('44');
+    expect(employee.email).toBe('example@gmail.com');
+})
